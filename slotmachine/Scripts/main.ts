18932d9e76a8b63aa7dc14a970d3f680a149e4c4
KevinDonkers <kevindonkers1@hotmail.com>
2014-10-15 18:57:25 -0400

    added image showing logic and some changes to power and reset logic

--- slotmachine/Scripts/main.ts
+++ slotmachine/Scripts/main.ts
@@ -6,13 +6,22 @@ var button300: createjs.Bitmap;
 var button400: createjs.Bitmap;
 var button500: createjs.Bitmap;
 var powerButton: createjs.Bitmap;
+var resetButton: createjs.Bitmap;
+var clear: createjs.Bitmap;
 var seven: createjs.Bitmap;
 var bar: createjs.Bitmap;
 var grape: createjs.Bitmap;
-var banana: createjs.Bitmap;
+var strawberry: createjs.Bitmap;
 var orange: createjs.Bitmap;
 var cherry: createjs.Bitmap;
 var bell: createjs.Bitmap;
+var startSeven1;
+var startSeven2;
+var startSeven3;
+var clear1;
+var clear2;
+var clear3;
+var blank: createjs.Bitmap;
 var betText: createjs.Text;
 var moneyText: createjs.Text;
 var jackpotText: createjs.Text;
@@ -29,7 +38,7 @@ var fruits = "";
 var winRatio = 0;
 
 var grapes = 0;
-var bananas = 0;
+var strawberries = 0;
 var oranges = 0;
 var cherries = 0;
 var bars = 0;
@@ -55,12 +64,20 @@ function drawSlotMachine() {
     var betBox = new createjs.Bitmap("images/textbox.png");
     var moneyBox = new createjs.Bitmap("images/textbox.png");
     var jackpotBox = new createjs.Bitmap("images/textbox.png");
-    var resetButton = new createjs.Bitmap("images/resetbutton.png");
-    powerButton = new createjs.Bitmap("images/powerbutton.png");
+    var textLabels = new createjs.Text("<-Money Jackpot->", "17px Arial", "#ffd900");
+    var betLabel = new createjs.Text("Bet->", "17px Arial", "#ffd900");
     betText = new createjs.Text("$" + playerBet.toString(), "30px Arial", "#ffd900");
     moneyText = new createjs.Text("$" + playerMoney.toString(), "30px Arial", "#ffd900");
     jackpotText = new createjs.Text("$" + jackpot.toString(), "30px Arial", "#ffd900");
-    spinHandle = new createjs.Bitmap("images/handle.png");
+    resetButton = new createjs.Bitmap("images/resetbutton.png");
+    powerButton = new createjs.Bitmap("images/powerbutton.png");
+    spinHandle = new createjs.Bitmap("images/handle.png"); 
+    startSeven1 = new createjs.Bitmap("images/7.png");
+    startSeven2 = new createjs.Bitmap("images/7.png");
+    startSeven3 = new createjs.Bitmap("images/7.png");
+    clear1 = new createjs.Bitmap("images/clear.png");
+    clear2 = new createjs.Bitmap("images/clear.png");
+    clear3 = new createjs.Bitmap("images/clear.png");   
     button100 = new createjs.Bitmap("images/button100.png");
     button200 = new createjs.Bitmap("images/button200.png");
     button300 = new createjs.Bitmap("images/button300.png");
@@ -68,23 +85,38 @@ function drawSlotMachine() {
     button500 = new createjs.Bitmap("images/button500.png");
     stage.enableMouseOver();
 
+    startSeven1.x = 40;
+    startSeven1.y = 236;
+
+    startSeven2.x = 174;
+    startSeven2.y = 236;
+
+    startSeven3.x = 306;
+    startSeven3.y = 236;
+
     betBox.x = 300;
     betBox.y = 367;
 
     betText.x = 310;
-    betText.y = 367;
+    betText.y = 366;
+
+    betLabel.x = 250;
+    betLabel.y = 372;
 
     moneyBox.x = 34;
     moneyBox.y = 182;
 
     moneyText.x = 44;
-    moneyText.y = 182;
+    moneyText.y = 181;
+
+    textLabels.x = 150;
+    textLabels.y = 190;
 
     jackpotBox.x = 300;
     jackpotBox.y = 182;
 
     jackpotText.x = 310;
-    jackpotText.y = 182;
+    jackpotText.y = 181;
 
     powerButton.x = 373;
     powerButton.y = 85;
@@ -95,27 +127,38 @@ function drawSlotMachine() {
     button100.x = 35;
     button100.y = 367;
 
-    button200.x = 85;
+    button200.x = 75;
     button200.y = 367;
 
-    button300.x = 135;
+    button300.x = 115;
     button300.y = 367;
 
-    button400.x = 185;
+    button400.x = 155;
     button400.y = 367;
 
-    button500.x = 235;
+    button500.x = 195;
     button500.y = 367;
 
     spinHandle.x = 443;
     spinHandle.y = 62;
 
+    clear1.x = 40;
+    clear1.y = 236;
+
+    clear2.x = 174;
+    clear2.y = 236;
+
+    clear3.x = 306;
+    clear3.y = 236;
+
     stage.addChild(slotMachine);
     stage.addChild(spinHandle);
     stage.addChild(betBox);
     stage.addChild(betText);
+    stage.addChild(betLabel);
     stage.addChild(moneyBox);
     stage.addChild(moneyText);
+    stage.addChild(textLabels);
     stage.addChild(jackpotBox);
     stage.addChild(jackpotText);
     stage.addChild(powerButton);
@@ -125,12 +168,30 @@ function drawSlotMachine() {
     stage.addChild(button300);
     stage.addChild(button400);
     stage.addChild(button500);
+    threeSevens();
 
+    addEventListeners(); 
+}
+
+function addEventListeners() {
     powerButton.addEventListener("click", function () {
         if (confirm("Are you sure you want to quit?")) {
             powerOff();
+            powerButton.removeAllEventListeners();
+            powerButton.addEventListener("click", addEventListeners);
+            powerButton.addEventListener("mouseover", function () {
+                powerButton.alpha = 0.5;
+            });
+            powerButton.addEventListener("mouseout", function () {
+                powerButton.alpha = 1;
+            });
         }
     });
+
+    betText.text = "$" + playerBet;
+    jackpotText.text = "$" + jackpot;
+    moneyText.text = "$" + playerMoney;
+
     powerButton.addEventListener("mouseover", function () {
         powerButton.alpha = 0.5;
     });
@@ -206,26 +267,20 @@ function drawSlotMachine() {
     });
 
     spinHandle.addEventListener("click", spin);
-    spinHandle.addEventListener("mouseover", overHandler);
-    spinHandle.addEventListener("mouseout", outHandler);
-    
-}
-
-function overHandler() {
-    spinHandle.alpha = 0.5;
-}
-
-function outHandler() {
-    spinHandle.alpha = 1;
+    spinHandle.addEventListener("mouseover", function () {
+        spinHandle.alpha = 0.5;
+    });
+    spinHandle.addEventListener("mouseout", function () {
+        spinHandle.alpha = 1;
+    });
 }
 
 function powerOff() {
+    resetAll();
     betText.text = "";
     jackpotText.text = "";
     moneyText.text = "";
-
-    powerButton.alpha = 1;
-    powerButton.removeAllEventListeners();
+    resetButton.removeAllEventListeners();
     spinHandle.removeAllEventListeners();
     button100.removeAllEventListeners();
     button200.removeAllEventListeners();
@@ -235,6 +290,7 @@ function powerOff() {
 }
 
 function spin() {
+
     if (playerMoney == 0) {
         if (confirm("You ran out of Money! \nDo you want to play again?")) {
             resetAll();
@@ -243,9 +299,26 @@ function spin() {
     else if (playerBet > playerMoney) {
         alert("You don't have enough Money to place that bet.");
     }
+    else if (playerBet == 0) {
+        alert("Please enter a valid bet amount");
+    }
     else if (playerBet <= playerMoney) {
+
+        clearReels();
+
         spinResult = Reels();
-        fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+
+        spinResult[0].x = 40;
+        spinResult[0].y = 236;
+
+        spinResult[1].x = 174;
+        spinResult[1].y = 236;
+
+        spinResult[2].x = 306;
+        spinResult[2].y = 236;
+
+        stage.addChild(spinResult[0], spinResult[1], spinResult[2]);
+
         determineWinnings();
         moneyText.text = "$" + playerMoney.toString();
         jackpotText.text = "$" + jackpot.toString();
@@ -253,10 +326,18 @@ function spin() {
     }
 }
 
+function clearReels() {
+    stage.addChild(clear1, clear2, clear3);
+}
+
+function threeSevens() {
+    stage.addChild(startSeven1, startSeven2, startSeven3);
+}
+
 /* Utility function to reset all fruit tallies */
 function resetFruitTally() {
     grapes = 0;
-    bananas = 0;
+    strawberries = 0;
     oranges = 0;
     cherries = 0;
     bars = 0;
@@ -278,6 +359,8 @@ function resetAll() {
     moneyText.text = "$" + playerMoney.toString();
     jackpotText.text = "$" + jackpot.toString();
     betText.text = "$" + playerBet.toString();
+    clearReels();
+    threeSevens();
 }
 
 
@@ -318,44 +401,44 @@ function checkRange(value, lowerBounds, upperBounds) {
 }
 
 /* When this function is called it determines the betLine results.
-e.g. Bar - Orange - Banana */
+e.g. Bar - Orange - Seven */
 function Reels() {
-    var betLine = [" ", " ", " "];
+    var betLine = [];
     var outCome = [0, 0, 0];
 
     for (var spin = 0; spin < 3; spin++) {
         outCome[spin] = Math.floor((Math.random() * 65) + 1);
         switch (outCome[spin]) {
             case checkRange(outCome[spin], 1, 27):  // 41.5% probability
-                betLine[spin] = "blank";
+                betLine[spin] = blank = new createjs.Bitmap("images/blank.png");
                 blanks++;
                 break;
             case checkRange(outCome[spin], 28, 37): // 15.4% probability
-                betLine[spin] = "Grapes";
+                betLine[spin] = grape = new createjs.Bitmap("images/Grape.png");
                 grapes++;
                 break;
             case checkRange(outCome[spin], 38, 46): // 13.8% probability
-                betLine[spin] = "Banana";
-                bananas++;
+                betLine[spin] = strawberry = new createjs.Bitmap("images/Grape.png");
+                strawberries++;
                 break;
             case checkRange(outCome[spin], 47, 54): // 12.3% probability
-                betLine[spin] = "Orange";
+                betLine[spin] = orange = new createjs.Bitmap("images/Grape.png");
                 oranges++;
                 break;
             case checkRange(outCome[spin], 55, 59): //  7.7% probability
-                betLine[spin] = "Cherry";
+                betLine[spin] = cherry = new createjs.Bitmap("images/Grape.png");
                 cherries++;
                 break;
             case checkRange(outCome[spin], 60, 62): //  4.6% probability
-                betLine[spin] = "Bar";
+                betLine[spin] = bar = new createjs.Bitmap("images/bar.png");
                 bars++;
                 break;
             case checkRange(outCome[spin], 63, 64): //  3.1% probability
-                betLine[spin] = "Bell";
+                betLine[spin] = bell = new createjs.Bitmap("images/Grape.png");
                 bells++;
                 break;
             case checkRange(outCome[spin], 65, 65): //  1.5% probability
-                betLine[spin] = "Seven";
+                betLine[spin] = seven = new createjs.Bitmap("images/7.png");
                 sevens++;
                 break;
         }
@@ -369,7 +452,7 @@ function determineWinnings() {
         if (grapes == 3) {
             winnings = playerBet * 10;
         }
-        else if (bananas == 3) {
+        else if (strawberries == 3) {
             winnings = playerBet * 20;
         }
         else if (oranges == 3) {
@@ -390,7 +473,7 @@ function determineWinnings() {
         else if (grapes == 2) {
             winnings = playerBet * 2;
         }
-        else if (bananas == 2) {
+        else if (strawberries == 2) {
             winnings = playerBet * 2;
         }
         else if (oranges == 2) {
